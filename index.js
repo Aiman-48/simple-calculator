c166c8781a520c1098cb8f7994f5bce7422fc82d
Aiman-48 <160266058+Aiman-48@users.noreply.github.com>
2024-04-03 19:08:44 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { messsage: "Enter first number", type: "number", name: "firstNumber" },
+    { messsage: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one option",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtratction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtratction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("plz select valid opertor");
+}
