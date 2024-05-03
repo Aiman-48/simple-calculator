e581d7372588b3bcbfdadc7548206c252ce8c081
Aiman-48 <160266058+Aiman-48@users.noreply.github.com>
2024-05-03 11:12:50 -0700

    new

--- index.ts
+++ index.ts
@@ -14,24 +14,13 @@ const answer = await inquirer.prompt([
 ]);
 
 if (answer.operator === "Addition") {
-
   console.log(answer.firstNumber + answer.secondNumber);
-
-} else if (answer.operator === "Subtratction"){
-
-    
+} else if (answer.operator === "Subtratction") {
   console.log(answer.firstNumber - answer.secondNumber);
-
-} else if (answer.operator === "Multiplication"){
-
- console.log(answer.firstNumber * answer.secondNumber);
-
-} else if (answer.operator === "Division"){
-
-    
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
   console.log(answer.firstNumber / answer.secondNumber);
 } else {
-
-    
   console.log("plz select valid opertor");
 }
